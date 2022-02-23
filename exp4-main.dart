a353db56fbcc5264e4d22edc1dd50451d18022a8
Abh1shekShinde <73304973+Abh1shekShinde@users.noreply.github.com>
2022-02-23 21:09:34 +0530

    Update exp4-main.dart

--- exp4-main.dart
+++ exp4-main.dart
@@ -6,173 +6,163 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  final appTitle = "Flutter Forms";
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: appTitle,
-    home:Scaffold(
-      appBar: AppBar(
-        title: Text(appTitle),
-      ),
-      body: MyForm(),
-    )
-  );
+    final appTitle = "Flutter Forms";
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: appTitle,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(appTitle),
+          ),
+          body: MyForm(),
+        ));
   }
 }
 
 class MyForm extends StatefulWidget {
   @override
-  MyFormState createState(){
+  MyFormState createState() {
     return MyFormState();
   }
 }
 
-class MyFormState extends State<MyForm>{
+class MyFormState extends State<MyForm> {
   final _formKey = GlobalKey<FormState>();
   var genderOptions = ['Male', 'Female', 'Others'];
   @override
   Widget build(BuildContext context) {
-  return Form(
-    key: _formKey,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        TextFormField(
-          decoration: InputDecoration(
-            icon: Icon(Icons.person),
-            hintText: 'Enter your name.',
-            labelText: 'Name',
-          ),
-          validator: (value) {
-            if (value!.isEmpty) {
-              return 'Please enter valid name';
-            }
-            return null;
-          },
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-            icon: Icon(Icons.account_box_sharp),
-            hintText: 'Enter your Roll number.',
-            labelText: 'PRN',
-          ),
-          validator: (value) {
-            if (value!.isEmpty) {
-              return 'Please enter correct PRN';
-            }
-            return null;
-          },
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-            icon: Icon(Icons.cake_outlined),
-            hintText: 'Enter your DOB.',
-            labelText: 'D.O.B.',
-          ),
-          validator: (value) {
-            if (value!.isEmpty) {
-              return 'Please enter correct DOB';
-            }
-            return null;
-          },
-        ),
-        FormBuilderDropdown(
-          name: 'Gender',
-          decoration: const InputDecoration(
-            labelText: 'Gender',
-              ),
-          allowClear: true,
-          hint: const Text("Select Gender"),
-          validator: FormBuilderValidators.compose(
-                  [
-                    FormBuilderValidators.required(context, errorText: "Select your gender")
-
-                  ]),
-              items: genderOptions
-                  .map((gender) => DropdownMenuItem(
-                        value: gender,
-                        child: Text('$gender'),
-                      ))
-                  .toList(),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.person),
+              hintText: 'Enter your name.',
+              labelText: 'Name',
             ),
-          
-      
-        FormBuilderChoiceChip(
-          name: 'choice',
-          validator: FormBuilderValidators.compose([
-               FormBuilderValidators.required(context , errorText: "Select your branch"),
-             ]), 
-          decoration:const InputDecoration(
-            labelText: "Select your branch",
-          ),
-          options: [
-            FormBuilderFieldOption(value: "CE", child: Text('CE')),
-            FormBuilderFieldOption(value: "IT", child: Text('IT')),
-            FormBuilderFieldOption(value: "EXTC", child: Text('EXTC')),
-            FormBuilderFieldOption(value: "MECH", child: Text('MECH')),
-          ],
-        ),
-
-        CheckboxListTileFormField(
-          title: Text('Agree to terms and condtions !'),
-          onSaved: (bool? value) {
-            print(value);
-          },
-          validator: (bool? value) {
-            if (value!) {
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter valid name';
+              }
               return null;
-            } else {
-              return 'This is required';
-            }
-          },
-          onChanged: (value) {
-            if (value) {
-              print("ListTile Checked :)");
-            } else {
-              print("ListTile Not Checked :(");
-            }
-          },
-          autovalidateMode: AutovalidateMode.always,
-          contentPadding: EdgeInsets.all(1),
-        ),
-
-          new Container(
-           padding: EdgeInsets.only(top: 20),
-         child: new RaisedButton(
-              
-              color: Theme.of(context).colorScheme.secondary,
-              child: Text(
-                "Reset",
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                _formKey.currentState!.reset();
-              },
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.account_box_sharp),
+              hintText: 'Enter your Roll number.',
+              labelText: 'PRN',
             ),
-    ),
-        new Container(
-          padding: EdgeInsets.only(top: 40),
-          child: new RaisedButton(
-            child: Text('Submit'),
-            onPressed: (){
-              if(_formKey.currentState!.validate()){
-                Scaffold.of(context)
-                    .showSnackBar(SnackBar(content: Text("Data is processing")));
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter correct PRN';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.cake_outlined),
+              hintText: 'Enter your DOB.',
+              labelText: 'D.O.B.',
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter correct DOB';
+              }
+              return null;
+            },
+          ),
+          FormBuilderDropdown(
+            name: 'Gender',
+            decoration: const InputDecoration(
+              labelText: 'Gender',
+            ),
+            allowClear: true,
+            hint: const Text("Select Gender"),
+            validator: FormBuilderValidators.compose([
+              FormBuilderValidators.required(context,
+                  errorText: "Select your gender")
+            ]),
+            items: genderOptions
+                .map((gender) => DropdownMenuItem(
+                      value: gender,
+                      child: Text('$gender'),
+                    ))
+                .toList(),
+          ),
+          FormBuilderChoiceChip(
+            name: 'choice',
+            validator: FormBuilderValidators.compose([
+              FormBuilderValidators.required(context,
+                  errorText: "Select your branch"),
+            ]),
+            decoration: const InputDecoration(
+              labelText: "Select your branch",
+            ),
+            options: [
+              FormBuilderFieldOption(value: "CE", child: Text('CE')),
+              FormBuilderFieldOption(value: "IT", child: Text('IT')),
+              FormBuilderFieldOption(value: "EXTC", child: Text('EXTC')),
+              FormBuilderFieldOption(value: "MECH", child: Text('MECH')),
+            ],
+          ),
+          CheckboxListTileFormField(
+            title: Text('Agree to terms and condtions !'),
+            onSaved: (bool? value) {
+              print(value);
+            },
+            validator: (bool? value) {
+              if (value!) {
+                return null;
+              } else {
+                return 'This is required';
               }
             },
+            onChanged: (value) {
+              if (value) {
+                print("ListTile Checked :)");
+              } else {
+                print("ListTile Not Checked :(");
+              }
+            },
+            autovalidateMode: AutovalidateMode.always,
+            contentPadding: EdgeInsets.all(1),
+          ),
+
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              RaisedButton(
+                color: Theme.of(context).colorScheme.secondary,
+                child: Text(
+                  "Reset",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  _formKey.currentState!.reset();
+                },
+              ),
+              RaisedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text("Data is processing")));
+                  }
+                },
+              )
+
+            ],
           )
-          
-          ) ,
-
-  
-      ],
-      
-    ),
-
-    
-  );
+        ],
+      ),
+    );
   }
 }
